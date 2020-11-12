b2c5eb0ff0aa852c5ba4fb3f1914d59661b98ee7
jomapormentilla <jomapormentilla@gmail.com>
2020-11-11 23:33:14 -0500

    commit tree 866f6a9ac1889481f26590d69be44e930d23e0a5

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,48 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    flatiron = Nokogiri::HTML(open(index_url))
+
+    students = []
+
+    flatiron.css(".student-card").each do |card|
+      hash = {
+        :name => card.css(".card-text-container .student-name").text,
+        :location => card.css(".card-text-container .student-location").text,
+        :profile_url => card.css("a").first["href"]
+      }
+      
+      students << hash
+    end
+
+    students
   end
 
   def self.scrape_profile_page(profile_url)
+    flatiron = Nokogiri::HTML(open(profile_url))
+
+    hash = {
+      :profile_quote => flatiron.css(".profile-quote").text,
+      :bio => flatiron.css(".description-holder p").text
+    }
     
+    flatiron.css(".social-icon-container a").each do |link|
+
+      link_format = link["href"].gsub("www.","").gsub(".com","")
+      link_arr = link_format.split("/")
+      
+      if link_arr.include?('twitter')
+        hash[:twitter] = link["href"]
+      elsif link_arr.include?('linkedin')
+        hash[:linkedin] = link["href"]
+      elsif link_arr.include?('github')
+        hash[:github] = link["href"]
+      elsif link_arr.include?('joemburgess')
+        hash[:blog] = link["href"]
+      end
+    end
+
+    hash
   end
 
 end
